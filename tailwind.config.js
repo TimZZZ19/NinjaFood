e41d0fefb49a0c38d28e0e875e00cdb9b9488130
TimZZZ19 <53802591+TimZZZ19@users.noreply.github.com>
2022-06-17 08:53:24 -0500

    added Nunito from GoogleFonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ['./public/**/*.{html,js}'],
   theme: {
     extend: {
       colors: {
-        primary: "#ff6363",
-        secondary: {
-          100: "#e2e2d5",
-          200: "#888883",
-        },
+        primary: '#ff6363',
+        secondary: { 100: '#e2e2d5', 200: '#888883' },
       },
+      fontFamily: { body: ['Nunito', 'sans-serif'] },
     },
   },
   plugins: [],
-};
+}
